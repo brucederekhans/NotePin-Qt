73856a41f698fbc5379716e51568f8f1efaa00b5
brucederekhans <brucederekhans@gmail.com>
2023-07-17 22:53:06 +0800

    pop up prompt to ask users if they want to close the window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,3 +21,30 @@ void MainWindow::resizeEvent(QResizeEvent * event)
     QMainWindow::resizeEvent(event);
 }
 
+void MainWindow::closeEvent(QCloseEvent * event)
+{
+    if(!ui->edtText->toPlainText().isEmpty())
+    {
+        QMessageBox * pMsgBox = new QMessageBox(nullptr);
+        pMsgBox->setText("文本框中存在文本，是否确认关闭？");
+        pMsgBox->setIcon(QMessageBox::Question);
+        pMsgBox->setStandardButtons(QMessageBox::Close | QMessageBox::Cancel);
+        pMsgBox->buttons().at(0)->setText("关闭");
+        pMsgBox->buttons().at(1)->setText("不关闭");
+        int buttunPressed = pMsgBox->exec();
+        delete pMsgBox;
+        if(buttunPressed == QMessageBox::Close)
+        {
+            event->accept();
+        }
+        else if(buttunPressed == QMessageBox::Cancel)
+        {
+            event->ignore();
+        }
+    }
+    else
+    {
+        event->accept();
+    }
+}
+
